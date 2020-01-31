16af837cc4f8351aef7a9111a7210773b9c3e693
Marin Balabanov <marincomics@gmail.com>
2020-01-31 20:04:31 +0200

    Added fake data

--- js/json-data.js
+++ js/json-data.js
@@ -1,14 +1,92 @@
 var teamMembers = `[
-        {"name":"Fran",
-        "email":"fran@index.com",
-        "image":"img/team/person1.png"},
+    {"name":"Eleonore Lautenschläger",
+    "email":"eleonore.lautenschlaeger@mail.com",
+    "hobby":"Tanzen",
+    "image":"img/person01.jpg"},
 
-        {"name":"Bill",
-        "email":"bill@index.com",
-        "image":"img/team/person2.png"},
+    {"name":"Laura Tegelmann",
+    "email":"laura.tegelmann@mail.com",
+    "hobby":"Lesen",
+    "image":"img/person02.jpg"},
 
-        {"name":"Fritz",
-        "email":"fritz@index.com",
-        "image":"img/team/person3.png"}
+    {"name":"Rene Hart",
+    "email":"rene.hart@mail.com",
+    "hobby":"Zeichnen",
+    "image":"img/person03.jpg"},
+
+    {"name":"Jasper Merz",
+    "email":"jasper.merz@mail.com",
+    "hobby":"Klettern",
+    "image":"img/person04.jpg"},
+
+    {"name":"Max Lohse",
+    "email":"max.lohse@mail.com",
+    "hobby":"Skateboarden",
+    "image":"img/person05.jpg"},
+
+    {"name":"Rebecca Störmer",
+    "email":"rebecca.stoermer@mail.com",
+    "hobby":"Fußball",
+    "image":"img/person06.jpg"},
+
+    {"name":"Julian Brosig",
+    "email":"julian.brosig@mail.com",
+    "hobby":"Tanzen",
+    "image":"img/person07.jpg"},
+
+    {"name":"Kira Lüdke",
+    "email":"kira.lüdke@mail.com",
+    "hobby":"Lesen",
+    "image":"img/person08.jpg"},
+
+    {"name":"Joana Velten",
+    "email":"joana.velten@mail.com",
+    "hobby":"Zeichnen",
+    "image":"img/person09.jpg"},
+
+    {"name":"Melissa Tauch",
+    "email":"melissa.tauch@mail.com",
+    "hobby":"Klettern",
+    "image":"img/person10.jpg"},
+
+    {"name":"Janna Baron",
+    "email":"janna.baron@mail.com",
+    "hobby":"Kampfsport",
+    "image":"img/person11.jpg"},
+
+    {"name":"Fred Glanz",
+    "email":"fred.glanz@mail.com",
+    "hobby":"Jonglieren",
+    "image":"img/person12.jpg"},
+
+    {"name":"Hanna Zenner",
+    "email":"hanna.zenner@mail.com",
+    "hobby":"Trampolinspringen",
+    "image":"img/person13.jpg"},
+
+    {"name":"Annelie Heiß",
+    "email":"annelie.heiss@mail.com",
+    "hobby":"Golf",
+    "image":"img/person14.jpg"},
+
+    {"name":"Hans-Joachim Funke",
+    "email":"hans-joachim.funke@mail.com",
+    "hobby":"Tischtennis",
+    "image":"img/person15.jpg"},
+
+    {"name":"Bodo Bleibtreu",
+    "email":"bodo.bleibtreu@mail.com",
+    "hobby":"Laufen",
+    "image":"img/person16.jpg"},
+
+    {"name":"Corinna Opel",
+    "email":"corinna.opel@mail.com",
+    "hobby":"Reisen",
+    "image":"img/person17.jpg"},
+
+    {"name":"Peter Brand",
+    "email":"peter.brand@mail.com",
+    "hobby":"Tischtennis",
+    "image":"img/person18.jpg",}
     ]`;
 
