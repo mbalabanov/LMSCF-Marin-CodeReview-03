4cf16a9bad43c06f4cf6a7ee9e0264a0dc12f06c
Marin Balabanov <marincomics@gmail.com>
2020-02-01 00:28:05 +0200

    Added Details for Cards

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,15 @@
-document.getElementById('calculateButton').addEventListener('click', function(event) {
-    event.preventDefault();
-    calculateInsurance();
-});
-
 let clientName = '';
 let clientCountry = '';
 let clientAge = '';
 let clientHP = '';
 let insurance = 0;
+
+function insuranceForm() {
+    document.getElementById('calculateButton').addEventListener('click', function(event) {
+        event.preventDefault();
+        calculateInsurance();
+    });
+}
 
 function calculateInsurance() {
     clientName = document.getElementById('formName').value;
@@ -33,4 +35,55 @@ function calculateInsurancePerCountry(clientCountry,clientAge,clientHP) {
     insurance = clientHP * factor / clientAge + addition;
 
     document.getElementById('output').innerHTML = '<p>' + clientName + ', your insurance costs ' + insurance.toFixed(2) + '€<br>(The insurance is calculated for ' + clientCountry + '.)</p>';
+}
+
+function generateTeam() {
+    let generatedDetails = '';
+    let generatedCards = '';
+    let generatedTeam = '';
+    let counter = 0;
+    for (let i in team.members) {
+        generatedDetails += `
+            <div class="teamMemberDetails invisible" id="details${team.members[i].id}">
+                <div class="teamMemberFlex">
+                    <div class="imageBox">
+                        <p><img src="${team.members[i].image}" alt="${team.members[i].name}"></p>
+                    </div>
+                    <div class="desciptionBox">
+                        <h3>${team.members[i].name}</h3>
+                        <h4>${team.members[i].job_title}</h4>
+                        <p>Contact: <a href="mailto:${team.members[i].email}">${team.members[i].email}</a></p>
+                        <p>Comes from <strong>${team.members[i].city}</strong>, and <strong>${team.members[i].hobby}</strong> is the favorite hobby.</p>
+                        <button class="detailsbutton" onclick="closeDetails(${team.members[i].id})">Close details</button>
+                    </div>
+                </div>
+            </div>
+        `;
+        generatedCards += `
+            <div class="teamMemberBox" id="card${team.members[i].id}">
+                <div class="label">
+                    <p><img src="${team.members[i].image}" alt="${team.members[i].name}" onclick="showDetails(${team.members[i].id})"></p>
+                    <h4>${team.members[i].name}</h4>
+                </div>
+                <button class="detailsbutton" onclick="showDetails(${team.members[i].id})">Show Details</button>
+            </div>
+        `;
+        counter++;
+        if (counter === 3) {
+            generatedTeam += generatedDetails;
+            generatedTeam += generatedCards;
+            generatedDetails = '';
+            generatedCards = '';
+            counter = 0;
+        }
+    };
+    document.getElementById('teamlist').innerHTML = generatedTeam;
+}
+
+function showDetails(id) {
+    document.getElementById('details' + id).classList.remove('invisible');
+}
+
+function closeDetails(id) {
+    document.getElementById('details' + id).classList.add('invisible');
 }
--- js/json-data.js
+++ js/json-data.js
@@ -1,92 +1,166 @@
-var teamMembers = `[
-    {"name":"Eleonore Lautenschläger",
-    "email":"eleonore.lautenschlaeger@mail.com",
-    "hobby":"Tanzen",
-    "image":"img/person01.jpg"},
-
-    {"name":"Laura Tegelmann",
-    "email":"laura.tegelmann@mail.com",
-    "hobby":"Lesen",
-    "image":"img/person02.jpg"},
-
-    {"name":"Rene Hart",
-    "email":"rene.hart@mail.com",
-    "hobby":"Zeichnen",
-    "image":"img/person03.jpg"},
-
-    {"name":"Jasper Merz",
-    "email":"jasper.merz@mail.com",
-    "hobby":"Klettern",
-    "image":"img/person04.jpg"},
-
-    {"name":"Max Lohse",
-    "email":"max.lohse@mail.com",
-    "hobby":"Skateboarden",
-    "image":"img/person05.jpg"},
-
-    {"name":"Rebecca Störmer",
-    "email":"rebecca.stoermer@mail.com",
-    "hobby":"Fußball",
-    "image":"img/person06.jpg"},
-
-    {"name":"Julian Brosig",
-    "email":"julian.brosig@mail.com",
-    "hobby":"Tanzen",
-    "image":"img/person07.jpg"},
-
-    {"name":"Kira Lüdke",
-    "email":"kira.lüdke@mail.com",
-    "hobby":"Lesen",
-    "image":"img/person08.jpg"},
-
-    {"name":"Joana Velten",
-    "email":"joana.velten@mail.com",
-    "hobby":"Zeichnen",
-    "image":"img/person09.jpg"},
-
-    {"name":"Melissa Tauch",
-    "email":"melissa.tauch@mail.com",
-    "hobby":"Klettern",
-    "image":"img/person10.jpg"},
-
-    {"name":"Janna Baron",
-    "email":"janna.baron@mail.com",
-    "hobby":"Kampfsport",
-    "image":"img/person11.jpg"},
-
-    {"name":"Fred Glanz",
-    "email":"fred.glanz@mail.com",
-    "hobby":"Jonglieren",
-    "image":"img/person12.jpg"},
-
-    {"name":"Hanna Zenner",
-    "email":"hanna.zenner@mail.com",
-    "hobby":"Trampolinspringen",
-    "image":"img/person13.jpg"},
-
-    {"name":"Annelie Heiß",
-    "email":"annelie.heiss@mail.com",
-    "hobby":"Golf",
-    "image":"img/person14.jpg"},
-
-    {"name":"Hans-Joachim Funke",
-    "email":"hans-joachim.funke@mail.com",
-    "hobby":"Tischtennis",
-    "image":"img/person15.jpg"},
-
-    {"name":"Bodo Bleibtreu",
-    "email":"bodo.bleibtreu@mail.com",
-    "hobby":"Laufen",
-    "image":"img/person16.jpg"},
-
-    {"name":"Corinna Opel",
-    "email":"corinna.opel@mail.com",
-    "hobby":"Reisen",
-    "image":"img/person17.jpg"},
-
-    {"name":"Peter Brand",
-    "email":"peter.brand@mail.com",
-    "hobby":"Tischtennis",
-    "image":"img/person18.jpg",}
-    ]`;
-
+var team = JSON.parse(`{
+    "members": [
+       {
+         "id": "1",
+         "name": "Eleonore Lautenschläger",
+         "job_title": "Investor Optimization Administrator",
+         "email": "eleonore.lautenschlaeger@mail.com",
+         "hobby": "Dancing",
+         "city": "Wien",
+         "image": "img/team/person01.jpg"
+       },
+       {
+         "id": "2",
+         "name": "Laura Tegelmann",
+         "job_title": "Customer Program Designer",
+         "city": "Graz",
+         "email": "laura.tegelmann@mail.com",
+         "hobby": "Reading",
+         "image": "img/team/person02.jpg"
+       },
+       {
+         "id": "3",
+         "name": "Rebecca Hart",
+         "job_title": "Senior Implementation Associate",
+         "email": "rebecca.hart@mail.com",
+         "hobby": "Painting",
+         "city": "Linz",
+         "image": "img/team/person03.jpg"
+       },
+       {
+         "id": "4",
+         "name": "Jasper Merz",
+         "job_title": "Central Accountability Engineer",
+         "email": "jasper.merz@mail.com",
+         "hobby": "Hiking",
+         "city": "Salzburg",
+         "image": "img/team/person04.jpg"
+       },
+       {
+         "id": "5",
+         "name": "Kira Lohse",
+         "job_title": "Global Usability Executive",
+         "email": "kira.lohse@mail.com",
+         "hobby": "Skating",
+         "city": "Innsbruck",
+         "image": "img/team/person05.jpg"
+       },
+       {
+         "id": "6",
+         "name": "Rene Störmer",
+         "job_title": "Product Factors Associate",
+         "email": "Rene.stoermer@mail.com",
+         "hobby": "Soccer",
+         "city": "Klagenfurt",
+         "image": "img/team/person06.jpg"
+       },
+       {
+         "id": "7",
+         "name": "Juliane Brosig",
+         "job_title": "Principal Solutions Architect",
+         "email": "juliane.brosig@mail.com",
+         "hobby": "Dancing",
+         "city": "Wels",
+         "image": "img/team/person07.jpg"
+       },
+       {
+         "id": "8",
+         "name": "Max Lüdke",
+         "job_title": "Dynamic Tactics Supervisor",
+         "email": "max.lüdke@mail.com",
+         "hobby": "Reading",
+         "city": "Sankt Pölten",
+         "image": "img/team/person08.jpg"
+       },
+       {
+         "id": "9",
+         "name": "Jochen Velten",
+         "job_title": "Senior Team Associate",
+         "email": "jochen.velten@mail.com",
+         "hobby": "Painting",
+         "city": "Dornbirn",
+         "image": "img/team/person09.jpg"
+       },
+       {
+         "id": "10",
+         "name": "Melissa Tauch",
+         "job_title": "Direct Web Consultant",
+         "email": "melissa.tauch@mail.com",
+         "hobby": "Hiking",
+         "city": "Wiener Neustadt",
+         "image": "img/team/person10.jpg"
+       },
+       {
+         "id": "11",
+         "name": "Janna Baron",
+         "job_title": "Internal Operations Orchestrator",
+         "email": "janna.baron@mail.com",
+         "hobby": "Martial Arts",
+         "city": "Steyr",
+         "image": "img/team/person11.jpg"
+       },
+       {
+         "id": "12",
+         "name": "Heidi Glanz",
+         "job_title": "Senior Marketing Consultant",
+         "email": "heidi.glanz@mail.com",
+         "hobby": "Juggling",
+         "city": "Feldkirch",
+         "image": "img/team/person12.jpg"
+       },
+       {
+         "id": "13",
+         "name": "Hanna Zenner",
+         "job_title": "Global Configuration Manager",
+         "email": "hanna.zenner@mail.com",
+         "hobby": "Trampoline jumping",
+         "city": "Bregenz",
+         "image": "img/team/person13.jpg"
+       },
+       {
+         "id": "14",
+         "name": "Annelie Heiß",
+         "job_title": "Investor Response Liason",
+         "email": "annelie.heiss@mail.com",
+         "hobby": "Golf",
+         "city": "Leonding",
+         "image": "img/team/person14.jpg"
+       },
+       {
+         "id": "15",
+         "name": "Corinna Funke",
+         "job_title": "Customer Optimization Assistant",
+         "email": "corinna.funke@mail.com",
+         "hobby": "Table tennis",
+         "city": "Klosterneuburg",
+         "image": "img/team/person15.jpg"
+       },
+       {
+         "id": "16",
+         "name": "Bodo Bleibtreu",
+         "job_title": "Interactive Metrics Coordinator",
+         "email": "bodo.bleibtreu@mail.com",
+         "hobby": "Running",
+         "city": "Baden",
+         "image": "img/team/person16.jpg"
+       },
+       {
+         "id": "17",
+         "name": "Hans-Joachim Opel",
+         "job_title": "Internal Resonance Specialist",
+         "email": "hans-oachim.opel@mail.com",
+         "hobby": "Traveling",
+         "city": "Wolfsberg",
+         "image": "img/team/person17.jpg"
+       },
+       {
+         "id": "18",
+         "name": "Peter Brand",
+         "job_title": "Corporate Research Strategist",
+         "email": "peter.brand@mail.com",
+         "hobby": "Table tennis",
+         "city": "Krems an der Donau",
+         "image": "img/team/person18.jpg"
+       }
+    ]
+ }`)
